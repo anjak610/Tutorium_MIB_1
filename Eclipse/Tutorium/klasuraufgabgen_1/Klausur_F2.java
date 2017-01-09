45b247ccecfde587ae47aa2745b62dfe16a03416
anjak610 <anja.siegrid.kretschmer@hs-furtwangen.de>
2017-01-09 16:36:00 +0100

    new exam exercises

---
+++ Eclipse/Tutorium/klasuraufgabgen_1/Klausur_F2.java
@@ -0,0 +1,15 @@
+package klasuraufgabgen_1;
+
+public class Klausur_F2 {
+	
+
+		public static void main(String[] args) {
+		int x[] = {2, 17, 5, 3, 4, 2};
+		int r = 0;
+		while (r > -1) {
+		System.out.println(r);
+		r = x[x[r]];
+		}
+		}
+		
+}
